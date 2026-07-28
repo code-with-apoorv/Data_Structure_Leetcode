c0f16cd5666cbd2ed9d3821871d91b8bb4b862e1
code-with-apoorv <160421486+code-with-apoorv@users.noreply.github.com>
2026-07-28 20:18:09 +0530

    Time: 24 ms (13.59%), Space: 45.2 MB (95.72%) - LeetHub

---
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length;
+
+        int[] dp = new int[n];
+        int[] count = new int[n];
+
+        // Every element itself is an LIS of length 1
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            count[i] = 1;
+        }
+
+        int maxi = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int prev = 0; prev < i; prev++) {
+
+                if (nums[prev] < nums[i] && dp[prev] + 1 > dp[i]) {
+                    dp[i] = dp[prev] + 1;
+                    count[i] = count[prev];
+                }
+                else if (nums[prev] < nums[i] && dp[prev] + 1 == dp[i]) {
+                    count[i] += count[prev];
+                }
+            }
+
+            maxi = Math.max(maxi, dp[i]);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == maxi) {
+                ans += count[i];
+            }
+        }
+
+        return ans;
+    }
+}
