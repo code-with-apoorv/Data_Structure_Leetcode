fd4096a8edf54a1b56a53dfcee15f6ffe70e6e65
code-with-apoorv <160421486+code-with-apoorv@users.noreply.github.com>
2026-09-05 21:44:17 +0530

    Time: 11 ms (64.83%), Space: 50.3 MB (37.69%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+         int low = 0;
+         int high = 0;
+         for(int i = 0; i < weights.length;i++){
+            low = Math.max(low,weights[i]);
+            high += weights[i];
+         }
+         while(low <= high){
+            int mid = low + (high - low) /2;
+            int reqday = 1;
+            int load = 0;
+            for(int i = 0;i < weights.length;i++){
+            if(load + weights[i] > mid){
+                reqday++;
+                load = weights[i];
+            }
+            else{
+                load += weights[i];
+            }
+            }
+            if(reqday <= days){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+         }
+         return low;
+    }
+}
