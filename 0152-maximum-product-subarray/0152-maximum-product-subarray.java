d4cedcf2bce81459ba62b7946660b4157947a001
code-with-apoorv <160421486+code-with-apoorv@users.noreply.github.com>
2026-09-11 23:32:01 +0530

    Time: 1 ms (93.32%), Space: 47 MB (87.6%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,13 +1,29 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max = Integer.MIN_VALUE;
+        // int max = Integer.MIN_VALUE;
+        // for(int i = 0; i < nums.length;i++){
+        // int product = 1;
+        // for(int j = i; j < nums.length;j++){
+        // product = product*nums[j];
+        // max = Math.max(product,max);
+        // }
+        // }
+        // return max;
+        int n = nums.length;
+        int max = Integer.MIN_VALUE; 
+        int prefix = 1;
+        int suffix = 1;
         for(int i = 0; i < nums.length;i++){
-        int product = 1;
-        for(int j = i; j < nums.length;j++){
-        product = product*nums[j];
-        max = Math.max(product,max);
-        }
+            if(prefix == 0){
+                prefix = 1;
+            }
+            if(suffix == 0){
+                suffix = 1;
+            }
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n-i-1];
+            max = Math.max(max,Math.max(prefix,suffix));
         }
         return max;
-    }
+         }
 }
